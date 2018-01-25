3c7315b4c13c7f95b4b52462fee553cc64958543
Francisco Santos <yoshi.df@gmail.com>
2018-01-25 01:33:43 -0200

    backend todo-app

---
+++ todo-app/backend/src/config/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+module.exports = function(server){
+
+	// API Routes
+	const router = express.Router()
+	server.use('/api', router)
+
+	// API Routes
+	const todoService = require('../api/todo/todo-service')
+	todoService.register(router, '/todos')
+}
