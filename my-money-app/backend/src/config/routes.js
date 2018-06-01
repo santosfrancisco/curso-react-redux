a8d16cc0def5af026f7e3425f11b7f3f8ac83a68
Francisco Santos <yoshi.df@gmail.com>
2018-06-01 19:10:39 -0300

    backend authentication with jwt

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const auth = require("./auth")
 
 module.exports = function(server){
 
-	const router = express.Router()
-	server.use('/api', router)
+  // api com validaçao de token
+	const protectedApi = express.Router()
+	server.use('/api', protectedApi)
 
-	// routes
 	const BillingCycle = require('../api/billing-cycle/billing-cycle-service')
-	BillingCycle.register(router, '/billingcycles')
+  BillingCycle.register(protectedApi, '/billingcycles')
+  
+  // api sem necessidade de validaçao de token
+  const openApi = express.Router()
+  serve.use("/oapi", openApi)
+
+  const authService = require("../api/user/authService")
+  openApi.post("/login", authService.login)
+  openApi.post("/signup", authService.signup)
+  openApi.post("/validatetoken", authService.validateToken)
 }
